20007d2f1887ca0b0e5ab0c39cebb8c54ecd5515
n313893254 <n313893254@126.com>
2019-08-14 11:36:05 +0800

    Bug fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Schema from './models/Schema'
 import Error from './models/Error'
 import Collection from './models/Collection'
 import createHttp from './utils/createHttp'
-import Socket from './core/Socket'
+import Websocket from './core/Socket'
 
 export {
   createHttp,
